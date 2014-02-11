6bade45ce2c6fd1521d4f7320df169878b62c32c
babodx <babodx@gmail.com>
2014-02-11 16:53:55 +0800

    增加邮件发送

--- check_record.py
+++ check_record.py
@@ -6,11 +6,36 @@ from optparse import OptionParser
 import telnetlib,time,os
 import types
 import datetime
-
+import smtplib
+from email.mime.text import MIMEText
 
 #get date
 now = datetime.datetime.now()
 day=now.strftime("%Y%m%d")
+
+#mail config
+mailto_list=["XXX@xxx.com"]
+mail_host="smtp.hysec.com"  #设置服务器
+mail_user="user"    #用户名
+mail_pass="pwd"   #口令
+mail_postfix="domain.com"  #发件箱的后缀
+
+def send_mail(to_list,sub,content):
+    me="<"+mail_user+"@"+mail_postfix+">"
+    msg = MIMEText(content,_subtype='plain',_charset='utf-8')
+    msg['Subject'] = sub
+    msg['From'] = me
+    msg['To'] = ";".join(to_list)
+    try:
+        server = smtplib.SMTP()
+        server.connect(mail_host)
+        server.login(mail_user,mail_pass)
+        server.sendmail(me, to_list, msg.as_string())
+        server.close()
+        return True
+    except Exception, e:
+        print str(e)
+        return False
 
 def check(host,user):
     now = datetime.datetime.now()
@@ -28,9 +53,9 @@ def check(host,user):
     tn.write(command.encode('UTF-8')+b"\r\n")
     result=tn.read_until(b'#',50)
     if (result.__len__() > 60):
-        print("ok")
+        send_mail(mailto_list,"check_record "+host+" success",host+" check status ok")
     else:
-        print("error")
+        send_mail(mailto_list,"check_record "+host+" fail",host+" check status error")
     #close
     tn.close() # tn.write('exit\n')
 
